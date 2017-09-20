2a190a74b486bd53c2f2ede32e08714eed71aced
Orrett Bailey <orrettlb@gmail.com>
2017-09-20 09:10:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,9 @@ var playlist = {
   song: "I look to you"
 }
 
+updatePlaylist(obj, key, value){
+  obj[key] = value
+  return obj
+}
+
 updatePlaylist(playlist, "Michael Jackson", "The way you make me feel")
