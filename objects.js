3fcbe8e1fc0201d8619884dfeecf74e00c79cf50
Orrett Bailey <orrettlb@gmail.com>
2017-09-20 09:18:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,12 +3,5 @@ var playlist = {
   song: "I look to you"
 }
 
-updatePlaylist(obj, key, value) {
-  obj[key] = value;
-  return obj;
-}
-const playlist = {
-  artist: "Whitney Houston",
-  song: "I look to you"
-}
+
 updatePlaylist(playlist, "Michael Jackson", "The way you make me feel")
